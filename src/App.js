5a1360fa3bda3d3bcc4a5efd6cf13e931c52aaff
Michal Stachnik <mstachnik@workstride.com>
2019-11-20 11:39:14 -0500

    Added button that adds new a new row to the grid with dummy data. Also prettier formatting.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,81 @@
 import React, { Component } from 'react';
 import './App.css';
 import { AgGridReact } from '@ag-grid-community/react';
-import {AllCommunityModules} from '@ag-grid-community/all-modules';
+import { AllCommunityModules } from '@ag-grid-community/all-modules';
 
 import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
 import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      columnDefs: [{
-        headerName: "Make", field: "make"
-      }, {
-        headerName: "Model", field: "model"
-      }, {
-        headerName: "Price", field: "price"
-      }],
-      rowData: [{
-        make: "Toyota", model: "Celica", price: 35000
-      }, {
-        make: "Ford", model: "Mondeo", price: 32000
-      }, {
-        make: "Porsche", model: "Boxter", price: 72000
-      }]
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			columnDefs: [
+				{
+					headerName: 'Make',
+					field: 'make'
+				},
+				{
+					headerName: 'Model',
+					field: 'model'
+				},
+				{
+					headerName: 'Price',
+					field: 'price'
+				}
+			],
+			rowData: [
+				{
+					make: 'Toyota',
+					model: 'Celica',
+					price: 35000
+				},
+				{
+					make: 'Ford',
+					model: 'Mondeo',
+					price: 32000
+				},
+				{
+					make: 'Porsche',
+					model: 'Boxter',
+					price: 72000
+				}
+			]
+		};
+	}
 
-  render() {
-    return (
-      <div 
-        className="ag-theme-balham"
-        style={{ 
-        height: '500px', 
-        width: '600px' }} 
-      >
-        <AgGridReact
-          columnDefs={this.state.columnDefs}
-          rowData={this.state.rowData}
-          modules={AllCommunityModules}>
-        </AgGridReact>
-      </div>
-    );
-  }
+	onGridReady = params => {
+		this.gridApi = params.api;
+	};
+
+	handleButtonClick = () => {
+		const newItem = {
+			make: 'Porsche',
+			model: 'Boxter',
+			price: 72000
+		};
+		this.gridApi.updateRowData({ add: [newItem] });
+	};
+
+	render() {
+		return (
+			<div
+				className="ag-theme-balham"
+				style={{
+					height: '500px',
+					width: '600px'
+				}}
+			>
+				<AgGridReact
+					columnDefs={this.state.columnDefs}
+					rowData={this.state.rowData}
+					modules={AllCommunityModules}
+					onGridReady={this.onGridReady}
+				></AgGridReact>
+				<button onClick={this.handleButtonClick}>Add Row</button>
+			</div>
+		);
+	}
 }
 
 export default App;
